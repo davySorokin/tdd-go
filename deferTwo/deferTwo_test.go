b2ca9802c142e53ac9a47dceb33ebeb3f00fdb3f
David <kirilldavidsorokin@gmail.com>
2026-03-05 14:21:12 +0800

    additional defer testing, since the return is outputted and then the defer is executed, this means that the defer still runs, but it is not shown in the 'got' variable hence not visible in the fmt.Fprint() call. So it works but looks like a fa l.

--- deferTwo/deferTwo_test.go
+++ deferTwo/deferTwo_test.go
@@ -3,7 +3,12 @@ package main
 import "testing"
 
 func TestDefTwo(t *testing.T) {
+
+	// technically the right output.
 	want := "Hello "
+
+	// Cannot output - defer outputs after return is executed.
+	//want := "Hello David"
 	got := deferTwo()
 
 	if want != got {
